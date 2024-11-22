80c12d7de8d8eb8a3e297308dd652eb858f47335
Arlindo Junior <arlindo.junior@sga.pucminas.br>
2024-11-22 18:06:45 -0300

    first commi

---
+++ src/main/java/email_service/emaill_service/EmaillServiceApplication.java
@@ -0,0 +1,13 @@
+package email_service.emaill_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmaillServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmaillServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/email_service/emaill_service/EmaillServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package email_service.emaill_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmaillServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
